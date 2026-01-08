7c57ca5af92c6e34a81013fdb444d686c6e6fb81
jan <152862650+j-haacker@users.noreply.github.com>
2026-01-08 14:08:00 +0100

    draft 1-input

---
+++ example_noninstallable_external_resource/noninstallable_module_example2.py
@@ -0,0 +1,2 @@
+def hello():
+    print("Hello from the non-installable example in a subdirectory")
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from coding-workshop-1!")
-
-
-if __name__ == "__main__":
-    main()
